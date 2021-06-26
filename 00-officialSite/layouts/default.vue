37514a55cc14b99b0e7c937e473fa506aaaa6f1e
DOCHIS <dochis2013@gmail.com>
2021-06-26 22:48:55 +0900

    사이트 추가

---
+++ 00-officialSite/layouts/default.vue
@@ -0,0 +1,50 @@
+<template>
+  <div>
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Noto+Sans+KR:wght@400;700&display=swap" rel="stylesheet">
+    <link href="https://unpkg.com/tailwindcss@^2/dist/tailwind.min.css" rel="stylesheet">
+
+    <!-- Navigation -->
+    <nav class="navbar fixed-top">
+      <div class="container sm:px-4 lg:px-8 flex flex-wrap items-center justify-between lg:flex-nowrap">
+        <a class="inline-block mr-4 py-0.5 text-xl whitespace-nowrap hover:no-underline focus:no-underline" href="/">
+          <img src="~/assets/images/logo.svg" alt="alternative" class="h-8">
+        </a>
+
+        <button class="background-transparent rounded text-xl leading-none hover:no-underline focus:no-underline lg:hidden lg:text-gray-400" type="button" data-toggle="offcanvas">
+          <span class="navbar-toggler-icon inline-block w-8 h-8 align-middle"></span>
+        </button>
+
+        <div id="navbarsExampleDefault" class="navbar-collapse offcanvas-collapse lg:flex lg:flex-grow lg:items-center">
+          <ul class="pl-0 mt-3 mb-2 ml-auto flex flex-col list-none lg:mt-0 lg:mb-0 lg:flex-row">
+            <li>
+              <a class="nav-link page-scroll active" href="/#header">홈 <span class="sr-only">(현재)</span></a>
+            </li>
+            <li>
+              <a class="nav-link page-scroll" href="/#details">기능</a>
+            </li>
+            <li>
+              <a class="nav-link page-scroll" href="/#private">개인정보처리방침</a>
+            </li>
+            <li>
+              <a class="nav-link page-scroll invite-cti" href="/#download">디스코드에 초대</a>
+            </li>
+          </ul>
+        </div> <!-- end of navbar-collapse -->
+      </div> <!-- end of container -->
+    </nav> <!-- end of navbar -->
+    <!-- end of navigation -->
+
+    <Nuxt />
+    <script src="~/assets/js/jquery.min.js"></script> <!-- jQuery for JavaScript plugins -->
+    <script src="~/assets/js/jquery.easing.min.js"></script> <!-- jQuery Easing for smooth scrolling between anchors -->
+    <script src="~/assets/js/swiper.min.js"></script> <!-- Swiper for image and text sliders -->
+    <script src="~/assets/js/jquery.magnific-popup.js"></script> <!-- Magnific Popup for lightboxes -->
+    <script src="~/assets/js/scripts.js"></script> <!-- Custom scripts -->
+  </div>
+</template>
+
+<style>
+body, html {font-family: 'Noto Sans KR', sans-serif;}
+</style>
---
+++ 00-officialSite/pages/index.vue
@@ -0,0 +1,155 @@
+<template>
+  <div>
+    <!-- Header -->
+    <header id="header" class="header py-28 text-center md:pt-36 lg:text-left xl:pt-44 xl:pb-32">
+      <div class="container px-4 sm:px-8 lg:grid lg:grid-cols-2 lg:gap-x-8">
+        <div class="mb-16 lg:mt-32 xl:mt-40 xl:mr-12">
+          <h1 class="h1-large mb-5">따봉도치야 고마워!</h1>
+          <p class="p-large mb-8">
+            언제나 당신에게 행운을 가져다주었던 따봉도치!<br>
+            이제 로스트아크를 위한 디스코드 봇으로만나보세요!<br>
+            따봉도치가 여러분들의 로생에 행운을 가져다줄꺼에요!
+          </p>
+          <a class="btn-solid-lg" href="/invite">
+            <i class="fab fa-discord"></i>
+            디스코드에 초대하기
+          </a>
+        </div>
+        <div class="xl:text-right">
+          <img class="inline" src="~/assets/images/header-smartphone.png" alt="alternative" />
+        </div>
+      </div> <!-- end of container -->
+    </header> <!-- end of header -->
+    <!-- end of header -->
+
+    <!-- Details 1 -->
+    <div id="details" class="pt-12 pb-16 lg:pt-16">
+      <div class="container px-4 sm:px-8 lg:grid lg:grid-cols-12 lg:gap-x-12">
+        <div class="lg:col-span-5">
+          <div class="mb-16 lg:mb-0 xl:mt-16">
+            <h2 class="mb-6">로스트아크의 다양한 수집물의 수집현황을 확인해보세요!</h2>
+            <p class="mb-4">
+              수집물 수집현황을 이제 명령어만으로 편하게 확인하세요!
+            </p>
+            <p class="mb-4">
+              <storng class="inline-strong" style="display:block">
+                <i class="fas fa-chevron-right color-primary"></i>
+                지원되는 명령어
+              </storng>
+              !섬마, !오페별, !거심, !미술품,<br>
+              !모코코, !모험물, !징표, !세계수
+            </p>
+          </div>
+        </div> <!-- end of col -->
+        <div class="lg:col-span-7">
+          <div class="xl:ml-14">
+            <img class="inline" src="~/assets/images/details-1.png" alt="명령어 종류" />
+          </div>
+        </div> <!-- end of col -->
+      </div> <!-- end of container -->
+    </div>
+    <!-- end of details 1 -->
+
+    <!-- Details 2 -->
+    <div class="py-24">
+      <div class="container px-4 sm:px-8 lg:grid lg:grid-cols-12 lg:gap-x-12">
+        <div class="lg:col-span-7">
+          <div class="mb-12 lg:mb-0 xl:mr-14">
+            <img class="inline" src="~/assets/images/details-2.png" alt="alternative" />
+          </div>
+        </div> <!-- end of col -->
+        <div class="lg:col-span-5">
+          <div class="xl:mt-12">
+            <h2 class="mb-6">로스트아크 인게임 이모티콘을 이제 디스코드에서도!!</h2>
+            <p class="mb-4">
+              게임 내에서 사용하던 이모티콘을 디스코드에서도 사용하고 싶으신가요??  채팅중 원하는 이모티콘을 괄호로 묶어서 입력해보세요! 따봉도치봇이 이모티콘을 올려줄꺼에요!
+            </p>
+            <p class="mb-4">
+              <storng class="inline-strong" style="display:block">
+                <i class="fas fa-chevron-right color-primary"></i>
+                지원되는 이모티콘
+              </storng>
+              [놀자에요], [머쓱해요], [뭐라구요], [사랑해], [웃기구요], [웃프네요], [정말이요], [추천이요], [화가나요], [화이팅]
+            </p>
+          </div>
+        </div> <!-- end of col -->
+      </div> <!-- end of container -->
+    </div>
+    <!-- end of details 2 -->
+
+    <!-- Details 3 -->
+    <div class="pt-16 pb-14" id="private">
+      <div class="container px-4 sm:px-8 lg:grid lg:grid-cols-12 lg:gap-x-12">
+        <div class="lg:col-span-5">
+          <div class="mb-16 lg:mb-0 xl:mt-16">
+            <h2 class="mb-6">개인정보를 어떻게 수집하고 폐기하는지 투명하게 안내하고 안전하게 관리하겠습니다</h2>
+            <p class="mb-4">
+              따봉도치봇은 채팅메시지를 분석하고 그에 따른 로봇 서비스 제공을 위해 채널의 채팅내역을 수집하고 목적을 달성한 이후에는 폐기합니다
+            </p>
+            <p class="mb-4">
+              따봉도치봇이 수집하는 정보를 어떻게 처리하는지에 대한 자세한 설명은 아래 링크를 참고해주세요!
+            </p>
+            <a class="btn-solid-reg popup-with-move-anim mr-1.5" href="/private">
+              어려운말 없이 적어본 개인정보처리방침!
+            </a>
+          </div>
+        </div> <!-- end of col -->
+        <div class="lg:col-span-7">
+          <div class="ml-14">
+            <img class="inline" src="~/assets/images/details-3.png" alt="alternative" />
+          </div>
+        </div> <!-- end of col -->
+      </div> <!-- end of container -->
+    </div>
+    <!-- end of details 3 -->
+
+    <!-- Conclusion -->
+    <div id="download" class="basic-5 mt-14">
+      <div class="container px-4 sm:px-8 lg:grid lg:grid-cols-2">
+        <div class="mb-16 lg:mb-0">
+          <img src="~/assets/images/conclusion-smartphone.png" alt="alternative" />
+        </div>
+        <div class="lg:mt-24 xl:mt-44 xl:ml-12">
+          <p class="mb-9 text-gray-800 text-3xl leading-10">
+            이제 따봉도치봇을<br>
+            디스코드 채널에<br>
+            초대해볼까요??
+          </p>
+          <a class="btn-solid-lg" href="/invite">
+            <i class="fab fa-discord"></i>
+            디스코드에 초대하기
+          </a>
+        </div>
+      </div> <!-- end of container -->
+    </div> <!-- end of basic-5 -->
+    <!-- end of conclusion -->
+
+    <!-- Footer -->
+    <div class="footer">
+      <div class="container px-4 pb-14 sm:px-8">
+        <h4 class="mb-8 lg:max-w-3xl lg:mx-auto">
+          따봉도치봇에 추가되었으면 하는 기능이나 사용중 불편사항이 있으신가요??
+          디스코드 채널에서 이야기해주세요!
+          <a class="text-indigo-600 hover:text-gray-500" href="https://discord.gg/HgzAUdJKwR" target="_blank">
+            https://discord.gg/HgzAUdJKwR
+          </a>
+        </h4>
+      </div> <!-- end of container -->
+    </div> <!-- end of footer -->
+    <!-- end of footer -->
+  </div>
+</template>
+
+<script>
+// export default {
+//   async asyncData ({ $http }) {
+//     const test = await $http.$get('/api/test')
+//     return {
+//       test
+//     }
+//   }
+// }
+</script>
+
+<style scoped>
+</style>
